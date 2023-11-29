a87c4018ac93e0938801a13c0b80cdb23d57dbbb
Hariss <harissbilal@hotmail.com>
2023-11-29 17:29:15 +0500

    fixed bugs

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-<<<<<<< HEAD
 from flask_login import LoginManager
-=======
->>>>>>> 022ef345da09b8407efbb019c3840aaa35c0036d
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -15,11 +12,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
-<<<<<<< HEAD
     
     
-=======
->>>>>>> 022ef345da09b8407efbb019c3840aaa35c0036d
     from .views import views
     from .auth import auth
     
@@ -28,7 +22,6 @@ def create_app():
     
     from .models import User, Note
     
-<<<<<<< HEAD
     
     with app.app_context():
         db.create_all()
@@ -40,9 +33,5 @@ def create_app():
     @Login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-=======
-    with app.app_context():
-        db.create_all()
->>>>>>> 022ef345da09b8407efbb019c3840aaa35c0036d
     
     return app
